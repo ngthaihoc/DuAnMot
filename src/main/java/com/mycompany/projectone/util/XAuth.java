0804cd5782710617c4eb73b400e8131cab2fd8c0
thaiHocng <ngthaihoc.vn@gmail.com>
2025-07-28 15:00:39 +0700

    Thêm các util có sẵn maybe giúp ích từ môn Dự Án Mẫu

---
+++ src/main/java/com/mycompany/projectone/util/XAuth.java
@@ -0,0 +1,23 @@
+    /*
+     * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+     * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+     */
+    package com.mycompany.projectone.util;
+
+//    import com.mycompany.cafe.entity.User;
+//    sửa link import lại sau khi khởi tạo packet entity
+    /**
+     *
+     * @author whath
+     */
+//    public class XAuth {
+//        public static User user = User.builder()
+//                .username("user1@gmail.com")
+//                .password("123")
+//                .enabled(true)
+//                .manager(true)
+//                .fullname("Nguyễn Văn Tèo")
+//                .photo("trump.png")
+//                .build(); // biến user này sẽ được thay thế sau khi đăng nhập
+//    }
+
