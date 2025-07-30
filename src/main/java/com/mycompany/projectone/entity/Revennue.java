5a2cc80f5584fc13d6b05fa0b57cdf0b7b0ba454
thaiHocng <ngthaihoc.vn@gmail.com>
2025-07-30 17:37:38 +0700

    đang code trước phần doanh thu, sẽ sữa lại entity sau khi done dl js-))

---
+++ src/main/java/com/mycompany/projectone/entity/Revennue.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectone.entity;
+
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/**
+ *
+ * @author WellyOwO
+ */
+public class Revennue {
+    
+ @AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+ 
+ public static class ByCategory{
+     
+     
+ }
+    
+}
