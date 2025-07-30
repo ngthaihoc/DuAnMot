c257ddb843b0d4eefb29fba90b71b2038f2e1f8d
thaiHocng <ngthaihoc.vn@gmail.com>
2025-07-30 09:11:04 +0700

    thêm vào các dao của entity, sẽ thêm dao impl sau

---
+++ src/main/java/com/mycompany/projectone/dao/CustomerDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.projectone.dao;
+
+import com.mycompany.projectone.entity.Customer;
+
+/**
+ *
+ * @author WellyOwO
+ */
+public interface CustomerDAO extends CurdDAO<Customer, Integer> {
+    
+}
