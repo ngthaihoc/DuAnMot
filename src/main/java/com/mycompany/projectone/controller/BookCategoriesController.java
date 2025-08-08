1fb3fc39186762f9c27d54ca797d8fe0e35da032
thaiHocng <ngthaihoc.vn@gmail.com>
2025-08-08 14:31:49 +0700

    controller done

---
+++ src/main/java/com/mycompany/projectone/controller/BookCategoriesController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.projectone.controller;
+
+import com.mycompany.projectone.entity.Categories;
+
+/**
+ *
+ * @author WellyOwO
+ */
+public interface BookCategoriesController extends CrudController<Categories>{
+    
+}
---
+++ src/main/java/com/mycompany/projectone/controller/RevenueManagerController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.projectone.controller;
+
+/**
+ *
+ * @author WellyOwO
+ */
+public interface RevenueManagerController {
+    void open(); // hiển thị doanh thu từng loại trong ngày
+    void selectTimeRange(); // hiển thị doanh thu theo khoảng thời gian được chọn
+    void fillRevenue();
+    
+}
