20fd90145c0e53d7eb73dd8e4e15024d9c4fdc61
hkhang3103 <70754142+phanhuukhang123@users.noreply.github.com>
2025-08-10 11:23:28 +0700

    Add files via upload

--- src/main/java/com/mycompany/projectone/dao/EmployeeDAO.java
+++ src/main/java/com/mycompany/projectone/dao/EmployeeDAO.java
@@ -5,11 +5,16 @@
 package com.mycompany.projectone.dao;
 
 import com.mycompany.projectone.entity.Employee;
+import java.util.List;
 
 /**
  *
  * @author WellyOwO
  */
 public interface EmployeeDAO extends CurdDAO<Employee, Integer> {
-    
+    Employee create(Employee entity);
+    void update(Employee entity);
+    void deletebyID(Integer entity);
+    List<Employee> findALl();
+    Employee FindByID(Integer id);
 }
